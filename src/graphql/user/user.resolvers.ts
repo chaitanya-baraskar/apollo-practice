d2691b563911122b35c4d00cc82a5ebf2dcf1f61
Chaitanya Baraskar <chaitanya.axe@gmail.com>
2024-08-03 16:21:08 +0530

    Adding mutations to register and login

--- src/graphql/user/user.resolvers.ts
+++ src/graphql/user/user.resolvers.ts
@@ -1,8 +1,42 @@
+import logger from "../../utils/logger";
+
+interface User {
+    username: string
+    password: string
+}
+
+interface AddUser {
+    id: string
+    firstname: string
+    lastname: string
+    username: string
+    role: string
+}
+
+
 export const userResolver = {
     Query: {
         users: () => [
-            {id: 1, firstName: "ABC", lastName: "DEF", userName: "test", role: "admin"}
+            {id: 1, firstname: "ABC", lastname: "DEF", username: "test", role: "admin"}
         ],
-        user: (_: any, {id}: any) => ({id: 1, firstName: "ABC", lastName: "DEF", userName: "test", role: "admin"}),
+        user: (_: any, {username}: any) => (
+            {id: 1, firstname: "ABC", lastname: "DEF", username: "test", role: "admin"}
+        ),
+    },
+    Mutation: {
+        login: (
+            _: any,
+            {username, password}: User
+        ) => {
+            console.log(`Received username ${username} and password ${password}`)
+            return {id: 1, firstName: "ABC", lastName: "DEF", userName: "test", role: "admin"}
+        },
+        registerUser: (
+            _: any,
+            userDetails: AddUser
+        ) => {
+            let strObject = JSON.stringify(userDetails)
+            logger.info(`Received ${strObject} as new user`)
+        }
     }
 }
--- src/graphql/user/user.schema.ts
+++ src/graphql/user/user.schema.ts
@@ -2,15 +2,80 @@ import {gql} from "apollo-server-express";
 
 export const userTypeDefs = gql`
     type User {
+        """
+        ID of the user.
+        """
         id: String
-        firstName: String,
-        lastName: String,
-        userName: String
+        
+        """
+        First name of the user
+        """
+        firstname: String,
+        
+        """
+        Last name of the user.
+        """
+        lastname: String,
+        
+        """
+        Username chosen by the user.
+        """
+        username: String
+        
+        """
+        Role of the user.
+        """
         role: String
     }
     
-    type Query {
+     type Query {
+        """
+        List of the users.
+        """
         users: [User]
-        user(id: ID!): User
+        
+        """
+        Filter user by ID.
+        """
+        user(username: String!): User
     }
+    
+    
+    """
+    Add new user in the system.
+    """
+    input AddUser{
+         """
+        First name of the user
+        """
+        firstname: String,
+        
+        """
+        Last name of the user.
+        """
+        lastname: String,
+        
+        """
+        Username chosen by the user.
+        """
+        username: String
+        
+        """
+        Role of the user.
+        """
+        role: String
+    }
+    
+     type Mutation {
+        """
+        Login user.
+        """
+        login(username: String!, password: String!): User
+        
+        """
+        Register new user.
+        """
+        registerUser(userDetails: AddUser): User
+    }
+    
 `;
